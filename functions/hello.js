9f9f61ac3ed6208278d02460c3606f413fc9bf5e
Viet Anh Le <mc.vietanh@gmail.com>
2021-10-28 21:02:28 -0400

    fixed

--- functions/hello.js
+++ functions/hello.js
@@ -2,7 +2,7 @@ let positionArr = [0, 0];
 // https://dreamy-heyrovsky-ef5242.netlify.app/.netlify/functions/hello?latitude=1&longitude=2
 
 function curPosition(latitude, longitude) {
-  if ((latitude, longitude)) {
+  if (latitude && longitude) {
     positionArr[0] = latitude;
     positionArr[1] = longitude;
   } else {
