9110a53c5d62e4f0e4c8ddd5d659167132197e2c
Viet Anh Le <mc.vietanh@gmail.com>
2021-10-28 20:55:22 -0400

    obj

--- map/index2.js
+++ map/index2.js
@@ -1,11 +1,19 @@
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+let showLocation = {};
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+fetch('https://dreamy-heyrovsky-ef5242.netlify.app/.netlify/functions/hello')
+  .then((res) => res.json())
+  .then((data) => (showLocation = data));
 
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-  .openPopup();
+console.log('🚀 ~ file: index2.js ~ line 3 ~ showLocation', showLocation);
+
+// var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution:
+//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+// }).addTo(map);
+
+// L.marker([51.5, -0.09])
+//   .addTo(map)
+//   .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//   .openPopup();
