693c370467493d22394a9a37b3c3e27c1c8005f9
Viet Anh Le <mc.vietanh@gmail.com>
2021-10-28 20:32:52 -0400

    updated

--- functions/hello.js
+++ functions/hello.js
@@ -2,8 +2,12 @@ let positionArr = [0, 0];
 // https://dreamy-heyrovsky-ef5242.netlify.app/.netlify/functions/hello?latitude=1&longitude=2
 
 function curPosition(latitude, longitude) {
-  positionArr[0] = latitude;
-  positionArr[1] = longitude;
+  if ((latitude, longitude)) {
+    positionArr[0] = latitude;
+    positionArr[1] = longitude;
+  } else {
+    return positionArr;
+  }
 }
 
 exports.handler = async (event) => {
